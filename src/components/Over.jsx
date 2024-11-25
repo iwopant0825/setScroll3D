e3ab0e13011fbacdc7f4d127f8ab3f8e1d6a00d4
iwopant0825 <129718228+iwopant0825@users.noreply.github.com>
2024-11-25 12:01:14 +0900

    moblie test

--- src/components/Over.jsx
+++ src/components/Over.jsx
@@ -26,65 +26,65 @@ export default function Over({setSceneAn,setSrcolltest}) {
     useFrame(() => {
       const scrollData = (Math.round(scroll.offset * 100) / 100) * 4 + 1;
       setSrcolltest(scrollData)
-      if (page == 1) {
-        if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-          setSceneAn(2)
-          scrollToSection(test2);
-        }
-      } else if (page == 2) {
-        if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-          setSceneAn(3)
-          scrollToSection(test3);
-        } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-          setSceneAn(1)
-          scrollToSection(test1);
-        }
-      } else if (page == 3) {
-        if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-          setSceneAn(4)
-          scrollToSection(test4);
-        } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-          setSceneAn(2)
-          scrollToSection(test2);
-        }
-      } else if (page == 4) {
-        if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-          setSceneAn(5)
-          scrollToSection(test5);
-        } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-          setSceneAn(3)
-          scrollToSection(test3);
-        }
-      } else if (page == 5) {
-        if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-          setSceneAn(4)
-          scrollToSection(test4);
-        }
-      }
+      // if (page == 1) {
+      //   if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
+      //     setSceneAn(2)
+      //     scrollToSection(test2);
+      //   }
+      // } else if (page == 2) {
+      //   if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
+      //     setSceneAn(3)
+      //     scrollToSection(test3);
+      //   } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
+      //     setSceneAn(1)
+      //     scrollToSection(test1);
+      //   }
+      // } else if (page == 3) {
+      //   if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
+      //     setSceneAn(4)
+      //     scrollToSection(test4);
+      //   } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
+      //     setSceneAn(2)
+      //     scrollToSection(test2);
+      //   }
+      // } else if (page == 4) {
+      //   if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
+      //     setSceneAn(5)
+      //     scrollToSection(test5);
+      //   } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
+      //     setSceneAn(3)
+      //     scrollToSection(test3);
+      //   }
+      // } else if (page == 5) {
+      //   if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
+      //     setSceneAn(4)
+      //     scrollToSection(test4);
+      //   }
+      // }
   
-      if (scrollData == 1) {
-        setpage(1);
-      } else if (scrollData == 2) {
-        setpage(2);
-      } else if (scrollData == 3) {
-        setpage(3);
-      } else if (scrollData == 4) {
-        setpage(4);
-      } else if (scrollData == 5) {
-        setpage(5);
-      }
+      // if (scrollData == 1) {
+      //   setpage(1);
+      // } else if (scrollData == 2) {
+      //   setpage(2);
+      // } else if (scrollData == 3) {
+      //   setpage(3);
+      // } else if (scrollData == 4) {
+      //   setpage(4);
+      // } else if (scrollData == 5) {
+      //   setpage(5);
+      // }
   
-      if (
-        scrollData !== 1 &&
-        scrollData !== 2 &&
-        scrollData !== 3 &&
-        scrollData !== 4 &&
-        scrollData !== 5
-      ) {
-        scroll.el.style.overflow = "hidden";
-      } else {
-        scroll.el.style.overflow = "auto";
-      }
+      // if (
+      //   scrollData !== 1 &&
+      //   scrollData !== 2 &&
+      //   scrollData !== 3 &&
+      //   scrollData !== 4 &&
+      //   scrollData !== 5
+      // ) {
+      //   scroll.el.style.overflow = "hidden";
+      // } else {
+      //   scroll.el.style.overflow = "auto";
+      // }
     });
   
     return (
